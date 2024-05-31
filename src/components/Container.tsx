64404b9a834069498e6aa7bc7ba492e5fbfb091f
sara_ahmadd <saraahmad2060039@gmail.com>
2024-05-31 21:39:47 +0300

    building issues

--- src/components/Container.tsx
+++ src/components/Container.tsx
@@ -1,7 +1,11 @@
 import { ReactNode } from "react";
 
 function Container({ children }: { children: ReactNode }) {
-  return <div className="container page-dimensions">{children}</div>;
+  return (
+    <div className="container">
+      <div className="page-dimensions">{children}</div>
+    </div>
+  );
 }
 
 export default Container;
