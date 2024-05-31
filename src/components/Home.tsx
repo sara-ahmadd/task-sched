df461c1a95023fe048a49e82606a892b805a76c5
sara_ahmadd <saraahmad2060039@gmail.com>
2024-05-31 21:55:06 +0300

    ui changes

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -15,7 +15,7 @@ function Home() {
     return (
       <div className="page-dimensions flex justify-center items-center">
         <div className="p-5 bg-white rounded-md shadow-lg flex flex-col justify-center items-center gap-5">
-          <p className="w-1/2 mx-auto text-center text-text font-medium ">
+          <p className="w-full md:w-1/2 mx-auto text-center text-text font-medium ">
             Stay Organized and Boost Your Productivity with Our Task Scheduler
             App. Plan, Track, and Achieve Your Goals Seamlessly
           </p>
